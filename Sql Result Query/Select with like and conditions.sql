7f395ebc8fcbccbb0c405f2498e9d2d996c2bd6c
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-02-29 10:54:52 +0500

    Lecture 1

---
+++ Sql Result Query/Select with like and conditions.sql
@@ -0,0 +1,11 @@
+--CREATE DATABASE books;
+
+--SELECT title FROM books;
+--SELECT * FROM books WHERE title = 'Still Born';
+--SELECT * FROM books WHERE title like 'S%';
+--SELECT * FROM books WHERE title like '%s%';
+--SELECT * FROM books WHERE title like '_y%';
+
+
+--SELECT * FROM books WHERE format = 'paperback' and year = 2024;
+--SELECT * FROM books WHERE pages >= 300 or year = 2024;
