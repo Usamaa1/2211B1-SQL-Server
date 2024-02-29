09fe25f4393b8044c318ad4c4366fa0967af5dcc
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-02-29 10:40:45 +0500

    Add files via upload

---
+++ lecture1db/publishers.sql
@@ -0,0 +1,39 @@
+CREATE TABLE [publishers] 
+(
+    [id]	INT,
+    [publisher]	VARCHAR(512)
+);
+
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('1', 'And Other Stories');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('2', 'Charco Press');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('3', 'Europa Editions');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('4', 'Faber & Faber');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('5', 'Fitzcarraldo Editions');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('6', 'Granta Books');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('7', 'Harvill Secker');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('8', 'Honford Star');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('9', 'Jonathan Cape');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('10', 'Literatura Random House');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('11', 'Lolli Editions');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('12', 'MacLehose Press');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('13', 'Oneworld');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('14', 'Peirene Press');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('15', 'Picador');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('16', 'Portobello Books');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('17', 'Pushkin Press');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('18', 'Riverrun');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('19', 'Sandstone Press');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('20', 'Scribe UK');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('21', 'Scribner');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('22', 'Seagull Books');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('23', 'Serpent''s Tail');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('24', 'Text Publishing');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('25', 'The New Press');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('26', 'Tilted Axis Press');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('27', 'Transit Books');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('28', 'Tuskar Rock');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('29', 'Verso Fiction');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('30', 'W&N');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('31', 'William Heinemann');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('32', 'World Editions');
+INSERT INTO [publishers] ([id], [publisher]) VALUES ('33', 'Yale University Press');
---
+++ lecture1db/sea_lions.sql
@@ -0,0 +1,13 @@
+CREATE TABLE sea_lions 
+(
+    id	INT PRIMARY KEY,
+    name	VARCHAR(512),
+    species	VARCHAR(512)
+);
+
+INSERT INTO sea_lions (id, name, species) VALUES ('10484', 'Ayah', 'Zalophus californianus');
+INSERT INTO sea_lions (id, name, species) VALUES ('11728', 'Spot', 'Zalophus californianus');
+INSERT INTO sea_lions (id, name, species) VALUES ('11729', 'Tiger', 'Zalophus californianus');
+INSERT INTO sea_lions (id, name, species) VALUES ('11732', 'Mabel', 'Zalophus californianus');
+INSERT INTO sea_lions (id, name, species) VALUES ('11734', 'Rick', 'Zalophus californianus');
+INSERT INTO sea_lions (id, name, species) VALUES ('11790', 'Jolee', 'Zalophus californianus');
