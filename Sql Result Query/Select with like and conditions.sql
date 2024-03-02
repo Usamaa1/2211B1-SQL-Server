46931d5fedb32c3867ed91f8b997d825690992e2
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-03-02 09:58:25 +0500

    Lecture 0 Completed

--- Sql Result Query/Select with like and conditions.sql
+++ Sql Result Query/Select with like and conditions.sql
@@ -9,3 +9,22 @@
 
 --SELECT * FROM books WHERE format = 'paperback' and year = 2024;
 --SELECT * FROM books WHERE pages >= 300 or year = 2024;
+
+
+SELECT * FROM books WHERE year BETWEEN 2020 AND 2023;
+
+SELECT * FROM books ORDER BY title DESC;
+
+
+SELECT COUNT(title) FROM  books WHERE year BETWEEN 2020 AND 2023;
+
+SELECT AVG(rating) FROM ratings;
+
+SELECT MAX(rating) FROM ratings;
+SELECT MIN(rating) FROM ratings;
+SELECT SUM(rating) FROM ratings;
+
+
+SELECT DISTINCT year FROM books;
+
+SELECT ROUND(2.4464554,0);
