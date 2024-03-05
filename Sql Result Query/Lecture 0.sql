e13ff7bbcac52cdf2383e8119937fb1525083c70
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2024-03-05 10:30:27 +0500

    Subqueries

--- Sql Result Query/Lecture 0.sql
+++ Sql Result Query/Lecture 0.sql
@@ -24,6 +24,59 @@ SELECT MAX(rating) FROM ratings;
 SELECT MIN(rating) FROM ratings;
 SELECT SUM(rating) FROM ratings;
 
+--SUBQURIES
+
+SELECT * FROM publishers;
+SELECT * FROM books;
+
+
+SELECT id FROM publishers WHERE publisher = 'Fitzcarraldo Editions';
+
+SELECT title FROM books WHERE publisher_id = 5;
+
+
+SELECT title FROM books WHERE
+	publisher_id = 
+	(
+		SELECT id FROM publishers WHERE
+		publisher = 'Fitzcarraldo Editions'
+	);
+
+
+
+SELECT id FROM books
+WHERE title = 'Flights';
+
+SELECT author_id FROM authored
+WHERE book_id = 78;
+
+SELECT name FROM authors
+WHERE id = 58;
+
+
+SELECT name FROM authors
+WHERE id = 
+	(
+		SELECT author_id FROM authored
+		WHERE book_id = 
+			(
+				SELECT id FROM books
+				WHERE title = 'Paradais'
+			)
+	)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 SELECT DISTINCT year FROM books;
 
